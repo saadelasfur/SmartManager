0d80c78490be53093074b0a0590414a004aa8c09
Saad <saadelasfur@gmail.com>
2024-11-29 15:58:57 +0300

    make_zip.sh: make the ZIP in out dir
    Signed-off-by: Saad <saadelasfur@gmail.com>

--- make_zip.sh
+++ make_zip.sh
@@ -26,18 +26,21 @@ VERIFY_7Z()
 MAKE_ZIP()
 {
     local SOURCE_DIR=$1
-    local OUTPUT_ZIP="${SOURCE_DIR}-$(date +%Y%m%d).zip"
-    local INSTALLER_ZIP="Installers/Installer.zip"
+    local SRC=$(pwd)
+    local OUT_DIR=$SRC/out
+    local OUTPUT_ZIP="$SOURCE_DIR-$(date +%Y%m%d).zip"
+    local INSTALLER_ZIP="$SRC/Installers/Installer.zip"
 
-    7z x "$INSTALLER_ZIP" -o"$SOURCE_DIR"
-    cat "Installers/$SOURCE_DIR/updater-script" > "$SOURCE_DIR/META-INF/com/google/android/updater-script"
-    cd "$SOURCE_DIR"
+    mkdir -p "$OUT_DIR/$SOURCE_DIR"
+    cp -a --preserve=all "$SRC/$SOURCE_DIR/." "$OUT_DIR/$SOURCE_DIR/"
+    7z x "$INSTALLER_ZIP" -o"$OUT_DIR/$SOURCE_DIR/"
+    cat "$SRC/Installers/$SOURCE_DIR/updater-script" > "$OUT_DIR/$SOURCE_DIR/META-INF/com/google/android/updater-script"
+    cd "$OUT_DIR/$SOURCE_DIR"
     find . -exec touch -a -c -m -t 200812311700.00 {} +
     7z a -tzip -mx=5 "$OUTPUT_ZIP" -x!"version" .
-    mv -f "$OUTPUT_ZIP" "../out/$OUTPUT_ZIP"
+    mv -f "$OUTPUT_ZIP" "$OUT_DIR/$OUTPUT_ZIP"
     echo "$OUTPUT_ZIP has been moved to out dir"
-    rm -rf META-INF
-    cd ..
+    cd $SRC
 }
 
 [[ -d "out" ]] && rm -rf out

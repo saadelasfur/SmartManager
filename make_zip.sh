f2978ef5dc20f18959d7658edcacc3808156aeea
Saad <saadelasfur@gmail.com>
2025-09-30 20:57:30 +0300

    make_zip.sh: sign output zip
    Signed-off-by: Saad <saadelasfur@gmail.com>

--- make_zip.sh
+++ make_zip.sh
@@ -9,15 +9,16 @@ MAKE_ZIP()
     local OUT_DIR="$SRC_DIR/out"
     local OUT_ZIP="$1-$(date +%Y%m%d).zip"
     local INSTALLER_ZIP="$SRC_DIR/Installers/Installer.zip"
+    export PATH="$SRC_DIR/.github/tools/bin:$PATH"
 
     mkdir -p "$OUT_DIR/$1"
-    cp -a --preserve=all "$SRC_DIR/$1/"* "$OUT_DIR/$1"
+    cp -a "$SRC_DIR/$1/"* "$OUT_DIR/$1"
     unzip -q "$INSTALLER_ZIP" -d "$OUT_DIR/$1"
     cat "$SRC_DIR/Installers/$1/updater-script" > "$OUT_DIR/$1/META-INF/com/google/android/updater-script"
     cd "$OUT_DIR/$1"
-    find . -exec env TZ=UTC touch -a -c -m -t 200901010000.00 {} \;
     zip -r -9 "$OUT_ZIP" . -x "version"
-    mv -f "$OUT_ZIP" "$OUT_DIR/$OUT_ZIP"
+    signapk -w "$SRC_DIR/signature/signature_testkey.x509.pem" "$SRC_DIR/signature/signature_testkey.pk8" \
+        "$OUT_ZIP" "$OUT_DIR/$OUT_ZIP"
     cd "$SRC_DIR"
 }
 # ]
@@ -31,5 +32,5 @@ MAKE_ZIP "SmartManagerCN" &> /dev/null
 echo "Making StockDeviceCare zip"
 MAKE_ZIP "StockDeviceCare" &> /dev/null
 
-echo "Build finished, exiting"
+echo "Build finished. Exiting..."
 sleep 1 && exit 0
